0a3d0b3f7ef04842e7fd67bea688aab5abc7cf61
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-10 19:29:46 +0530

    signup functionality created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDTO } from './create-user.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('/signin')
-  getTasks(): string {
-    return `hello`;
+  @Post('/signup')
+  createUser(@Body() createUserDTO: CreateUserDTO): Promise<User> {
+    return this.authService.createUser(createUserDTO);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDTO } from './create-user.dto';
 import { User } from './user.entity';
 import { UserRepository } from './users.repository';
 
@@ -9,4 +10,15 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: UserRepository,
   ) {}
+
+  async createUser(createUserDTO: CreateUserDTO): Promise<User> {
+    const { username, password } = createUserDTO;
+
+    const user = this.userRepository.create({
+        username,
+        password
+    });
+    await this.userRepository.save(user);
+    return user;
+  }
 }
