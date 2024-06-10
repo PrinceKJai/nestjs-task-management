04c45e53c914982b535754491020c4acd95d26ae
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-10 19:34:54 +0530

    validation for DTO

--- src/auth/create-user.dto.ts
+++ src/auth/create-user.dto.ts
@@ -1,9 +1,16 @@
-import {IsNotEmpty } from 'class-validator';
+import {IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDTO {
     @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
     
     @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(30)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
     password: string;
 }
