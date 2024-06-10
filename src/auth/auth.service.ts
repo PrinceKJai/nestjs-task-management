4fd88da325a60eb762ae1822cc6bbf1803309339
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-10 15:51:55 +0530

    dependency injection 4 service and controller auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './users.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: UserRepository,
+  ) {}
+}
