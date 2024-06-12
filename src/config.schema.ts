794af05a1780495bc4b157c62a484c5046b56b3e
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-12 17:41:37 +0530

    schema validators for config files

---
+++ src/config.schema.ts
@@ -0,0 +1,13 @@
+import Joi from "@hapi/joi";
+
+//Created this validation schema function for scenarios where we forgot to provide 
+//config variables values then we do not have a proper error message to know what went wrong.
+//This schema validation will help us know the exact issues with config files, if any
+export const ConfigValidationSchema = Joi.object({
+ STAGE: Joi.string().required(),
+ DB_HOST: Joi.string().required(),
+ DB_PORT: Joi.number().default().required(),
+ DB_USERNAME: Joi.string().required(),
+ DB_PASSWORD: Joi.string().required(),
+ DB_DATABASE: Joi.string().required(),
+});
